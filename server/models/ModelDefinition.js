c27115f4f2c7fecd981970fba508e90e63428beb
Ritu Ravindra Raut <ravindraraur@vmware.com>
2019-11-04 12:49:42 +0530

    1. ModelDefinition and ServiceDefinition generation 2. Added ModelReference

--- server/models/ModelDefinition.js
+++ server/models/ModelDefinition.js
@@ -3,7 +3,6 @@ var mongoose = require("mongoose");
 var ModelDefinitionSchema = new mongoose.Schema({
   service_id : String,
   model_name: String,
-  model_references: [mongoose.Schema.Types.ObjectId],
   model_obj: mongoose.Schema.Types.Mixed
 });
 
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,9 +1,13 @@
-const Dependency = require('../models/Dependency')
+//call the APIs for generation of these models one by one from the UI side.
+const ServiceDefinition = require('../models/ServiceDefinition')
 const ModelDefinition = require('../models/ModelDefinition')
 const ModelTestData = require('../models/ModelTestData')
 const PathDefinition = require('../models/PathDefinition')
+const ModelReference = require('../models/ModelReference')
+const Dependency = require('../models/Dependency')
 const accountUtil = require('../controller/accountUtil')
 const crypto = require('crypto')
+const fs = require("fs");
 // const jwt = require('jsonwebtoken');
 // const moment = require('moment');
 // const NodeGeocoder = require('node-geocoder');
@@ -17,6 +21,42 @@ var connect = require('connect');
 
 
 module.exports = function(app) {
+
+    // Service Definition routes
+    app.get('/service_defs', function(req, res) {
+        ServiceDefinition.find(function(err, service_def){
+            res.json(service_def).end();
+        })
+    });
+
+    // app.post('/service_def/:id', function(req, res) {
+    //     ModelDefinition.remove({_id: req.params.id}, function(err, result){
+    //         if(err){
+    //             res.json(err)
+    //         }
+    //         else{
+    //             res.json(result)
+    //         }
+    //     })
+    // });
+
+    app.post('/service_def', function(req, res) {
+        let newServiceDef = new ServiceDefinition({
+            service_name : req.body.service_name,
+            version: req.body.version,
+            description: req.body.description
+        })
+        newServiceDef.save(function(err, model_def){
+            if(err){
+                res.json({msg: 'Failed to add model definition.'})
+            }
+            else{
+                res.json({msg: 'Model definition added successfully.'})
+            }
+        })
+    });
+    
+    // Model Definitions routes
 
     app.get('/hello', function(req, res) {
 	if(!req.user) {
@@ -47,18 +87,100 @@ module.exports = function(app) {
                 res.json({msg: 'Model definition added successfully.'})
             }
         })
-});
+    });
 
-app.delete('/model_def/:id', function(req, res) {
-    ModelDefinition.remove({_id: req.params.id}, function(err, result){
-        if(err){
-            res.json(err)
-        }
-        else{
-            res.json(result)
-        }
-    })
-});
+    app.delete('/model_def/:id', function(req, res) {
+        ModelDefinition.remove({_id: req.params.id}, function(err, result){
+            if(err){
+                res.json(err)
+            }
+            else{
+                res.json(result)
+            }
+        })
+    });
 
+    app.post('/generate_service_def_model_defs', function(req, res) {
+        //get service id first and use it from req body
+        var jsonContent = JSON.parse(fs.readFileSync('server/routes/PetstoreAPISpec.json', 'utf8').trim());
+        let newServiceDef = new ServiceDefinition({
+            service_name : jsonContent.info.title,
+            version: jsonContent.info.version,
+            description: jsonContent.info.description
+        })
+        newServiceDef.save(function(err, service_def){
+            if(err){
+                res.json({msg: 'Failed to add service definition.'})
+            }
+            else{
+                var definitions = jsonContent.definitions
+                var model_arr = []
+                for(var model in definitions) {
+                    for (var key in definitions[model]){
+                        if ("xml" == key){
+                            delete definitions[model]["xml"]
+                        }
+                    }
+                    for(var key in definitions[model].properties){
+                        for(var nested_key in definitions[model].properties[key]){
+                            if ("xml" == nested_key){
+                                delete definitions[model].properties[key]["xml"]
+                            }                            
+                        }
+                    }
+                    let newModelDef = new ModelDefinition({
+                        service_id : newServiceDef.id,
+                        model_name: model,
+                        model_obj: definitions[model]
+                    })
+                    model_arr.push(newModelDef)
+                }
+                ModelDefinition.insertMany(model_arr, function(err, model_defs){
+                    if(err){
+                        res.json({msg: 'Failed to add model definitions.'})
+                    }
+                    else{
+                        console.log(model_defs)
+                        res.json({msg: 'Model definitions added successfully.'})
+                    }
+                })
+            }
+            console.log(service_def)
+        })        
+    });
+
+// Model Test Data
+    // app.post('/model_test_data', function(req, res) {
+    //     let newModelTestData = new ModelTestData({
+    //         service_id : req.body.service_id,
+    //         model_name: req.body.model_name,
+    //         model_references: req.body.model_references,
+    //         model_obj: req.body.model_obj
+    //     })
+    //     newModelDef.save(function(err, model_def){
+    //         if(err){
+    //             res.json({msg: 'Failed to add test data.'})
+    //         }
+    //         else{
+    //             res.json({msg: 'Model test data added successfully.'})
+    //         }
+    //     })
+    // });
+
+    //Model references
+    app.post('/model_ref', function(req, res) {
+        let newModelRef = new ModelReference({
+            service_id : req.body.service_id,
+            model_id: req.body.model_id
+        })
+        newModelRef.save(function(err, model_def){
+            if(err){
+                res.json({msg: 'Failed to add model Reference.'})
+            }
+            else{
+                res.json({msg: 'Model Reference added successfully.'})
+            }
+        })
+    });
 }
 
